1102f525b3e3b6bf17827ddbd6f04c982a018a25
Austin Story <astory@doximity.com>
2017-01-01 13:43:47 -0600

    Exposing greetr to outside world

--- greetr.js
+++ greetr.js
@@ -1,5 +1,18 @@
-greetr = (function(global, $) {
+(function(global, $) {
 
+  Greetr = function(first, last, lang) {
+    return new Greetr.init(first, last, lang)
+  };
 
+  Greetr.init = function(first, last, lang) {
+    this.first = first || 'default';
+    this.last = last || 'default';
+    this.lang = lang || 'en';
+  };
+
+  Greetr.prototype = {};
+  Greetr.init.prototype = Greetr.prototype;
+
+  global.Greetr = global.G$ = Greetr;
 
 }(global, jQuery));
