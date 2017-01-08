9e767d18a8ca0ab5a566a0ab5b2f0c793e3c2550
Austin Story <astory@doximity.com>
2017-01-07 21:17:24 -0600

    Exercise 5

---
+++ VUE/Exercise 5 - vFor/app.js
@@ -0,0 +1,22 @@
+new Vue({
+  el: '#exercise',
+  data: {
+    array: ['Max', 'Anna', 'Chris', 'Manu'],
+    myObject: {
+      title: 'Lord of the Rings',
+      author: 'J.R.R. Tolkiens',
+      books: '3'
+    },
+    testData: {
+      name: 'TESTOBJECT',
+      id: 10,
+      data: [1.67, 1.33, 0.98, 2.21]
+    },
+    toggle: false,
+  },
+  methods: {
+    toggleMe: function() {
+      this.toggle = !this.toggle;
+    }
+  }
+});
