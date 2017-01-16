88331d2d6a29a280608daa5bb47afe5dc7784d71
Austin Story <astory@doximity.com>
2017-01-16 12:12:25 -0600

    Dynamically select displayed component on click

---
+++ VUE/Exercise 8 - Dynamic Components/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary" @click="setComponent('app-blue')">Load Blue Template</button>
+                <button class="btn btn-success" @click="setComponent('app-green')">Load Green Template</button>
+                <button class="btn btn-danger" @click="setComponent('app-red')">Load Red Template</button>
+                <hr>
+                <component :is="selectedComponent">
+                    <h1> This Title </h1>
+                </component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        },
+        methods: {
+          setComponent(comp){
+            this.selectedComponent = comp;
+            return true;
+          }
+        },
+        data() {
+          return {
+            selectedComponent: 'app-green',
+          }
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ VUE/Exercise 8 - Dynamic Components/src/components/Green.vue
@@ -0,0 +1,18 @@
+<template>
+<div><slot></slot1</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid green;
+        background-color: lightgreen;
+        padding: 30px;
+        margin: 20px auto;
+        text-align: center;
+        color: red;
+    }
+</style>
