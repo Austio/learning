561b4004c1ca3f67a32793c064655493cc095aa4
Austin Story <astory@doximity.com>
2017-01-17 22:07:22 -0600

    Add instructor example

--- VUE/Exercise 10 - Custom Von/src/App.vue
+++ VUE/Exercise 10 - Custom Von/src/App.vue
@@ -5,14 +5,35 @@
                 <h1>Directives Exercise</h1>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-
+               <div v-my-on:click="fooFunction">My Custom Directive!</div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+      directives: {
+        'my-on': {
+          bind(el, bindings, vnode){
+            let listener = bindings.arg;
+            let func = vnode.context[bindings.expression];
+
+            el.addEventListener(listener, func)
+          }
+        }
+      },
+      data() {
+        return {
+          fooValue: 'Howdy',
+        }
+      },
+      methods: {
+        fooFunction() {
+          alert(this.fooValue);
+        }
+      }
+    }
 </script>
 
 <style>
