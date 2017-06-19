42400a63326bbb8dc8d9bd76dcb33a59ab63d91b
Austin Story <astory@doximity.com>
2017-06-18 22:21:21 -0500

    Remove testbed

--- Testbed/vue/src/routes.js
+++
@@ -1,8 +0,0 @@
-import Hello from './components/Hello';
-import Newsfeed from './components/newsfeed/Newsfeed';
-
-
-export default [
-  { path: '/newsfeed', name: 'newfeed', component: Newsfeed },
-  { path: '', default: true, name: 'home', component: Hello },
-];
