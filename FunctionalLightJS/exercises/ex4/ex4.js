adf6f340d6a1a37892a1f7c2b9fee65317193a16
Austin Story <lonnieastory@gmail.com>
2017-09-19 22:35:28 -0500

    FunctionJS commit

--- FunctionalLightJS/exercises/ex4/ex4.js
+++ FunctionalLightJS/exercises/ex4/ex4.js
@@ -2,7 +2,17 @@ function lotteryNum() {
 	return (Math.round(Math.random() * 100) % 58) + 1;
 }
 
-function pickNumber(){}
+function pickNumber(){
+	const length = luckyLotteryNumbers.length;
+
+	while (luckyLotteryNumbers.length === length) {
+		let num = lotteryNum();
+
+		if (luckyLotteryNumbers.indexOf(num) === -1) {
+      luckyLotteryNumbers = [...luckyLotteryNumbers, num].sort();
+    }
+	}
+}
 
 var luckyLotteryNumbers = [];
 
