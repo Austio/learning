06e22983cc4140d2132aca277d81824f587636c2
Austin Story <lonnieastory@gmail.com>
2017-09-19 22:46:06 -0500

    FunctionalJS Handle recursivie challenge

--- FunctionalLightJS/exercises/ex6/ex6.js
+++ FunctionalLightJS/exercises/ex6/ex6.js
@@ -1,7 +1,16 @@
-function mult(x,y,z) {
-	return x * y * z;
+function mult(...args) {
+  if (args.length < 1) {
+    return 1;
+  }
+	else if (args.length === 1) {
+	  return args[0];
+  } else {
+	  return args[0] * mult(...args.splice(1))
+  }
 }
 
-mult(3,4,5);	// 60
+console.log(mult(3))
+console.log(mult(3,4))
+console.log(mult(3,4,5));	// 60
 
-mult(3,4,5,6);	// Oops!
+console.log(mult(3,4,5,6));	// Oops!
