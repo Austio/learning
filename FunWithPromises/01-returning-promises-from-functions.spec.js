5861967b688ca17a179b96bcbdbe06c73cae8629
Austin <lonnieastory@gmail.com>
2017-10-01 22:54:37 -0500

    Demonstrating using promises with a delay and emitting event

--- FunWithPromises/01-returning-promises-from-functions.spec.js
+++ FunWithPromises/01-returning-promises-from-functions.spec.js
@@ -1,3 +1,6 @@
+const EventEmitter = require('events');
+class MyEmitter extends EventEmitter {}
+
 describe('Properly Returning Promise from Function', () => {
   let calls;
 
@@ -14,12 +17,11 @@ describe('Properly Returning Promise from Function', () => {
   });
 
   const pushV = v => {
-    console.log(v);
     calls.push(v);
     return v;
   }
 
-  xdescribe('helpers', () => {
+  describe('helpers', () => {
     it('sumArgs', () => {
       expect(sumArgs(1,2,3,4,5)).toEqual(15);
     });
@@ -36,20 +38,59 @@ describe('Properly Returning Promise from Function', () => {
     })
   });
 
-  it('Immediately Invokes Promises when they are defined', () => {
+  it('Immediately Invokes Promises when they are defined', (done) => {
     const p1 = Promise.resolve(1).then(pushV);
     const p2 = Promise.resolve(2).then(pushV);
 
     return Promise.all([p1, p2])
       .then(sumArgs)
-      .then(v => {
-        console.log('sum', v);
-        return v;
-      })
       .then(pushV)
       .then((v) => {
-        console.log(v);
         expect(calls).toEqual([1,2,3]);
+        done();
       });
+  });
+
+  it('Only resolves promises when we enter their execution context', (done) => {
+    const p1 = () => Promise.resolve(1).then(pushV);
+    const p2 = () => Promise.resolve(2).then(pushV);
+
+    pushV('ShouldBeFirst');
+
+    return Promise.all([p1(), p2()])
+      .then(sumArgs)
+      .then(pushV)
+      .then((v) => {
+        expect(calls).toEqual(['ShouldBeFirst',1,2,3]);
+        done();
+      });
+  });
+  
+  it('Works with emits stuff with a delay', (done) => {
+    const getPromisesArray = () => Promise.all([
+      Promise.resolve(1).then(pushV),
+      Promise.resolve(2).then(pushV),
+    ]);
+
+    const router = new EventEmitter();
+
+    const delayedResolve = new Promise((resolve) => {
+      setTimeout(function(){
+          resolve('delayedResolveDone')}
+        , 2500)
+    })
+
+    const promise = new Promise((resolve, reject) => {
+      resolve(delayedResolve.then(pushV));
+    });
+
+    router.on('ready', () => {
+      return promise.then(() => getPromisesArray().then(() => {
+        expect(calls).toEqual(['delayedResolveDone', 1, 2]);
+        done()
+      }))
+    });
+
+    router.emit('ready');
   })
 })
