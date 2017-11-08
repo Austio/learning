da248c40ca3cdcca0cbe8fb82f323e99d23cd878
Austin Story <lonnieastory@gmail.com>
2017-11-08 06:44:55 -0600

    Example of not respecting return from constructor and allowing arguments

--- FunWithConstructors/01-creating-objects-and-prototypes.spec.js
+++ FunWithConstructors/01-creating-objects-and-prototypes.spec.js
@@ -9,5 +9,25 @@ describe('constructors', () => {
     expect(typeof john).toBe('object');
     expect(john.name).toBe('john');
   });
+
+  it('does not respect the return word', () => {
+    function Person() {
+      this.name = 'john'
+
+      return 1;
+    }
+
+    const john = new Person();
+    expect(john.name).toBe('john');
+  });
+
+  it('allows arguments', () => {
+    function Person(name) {
+      this.name = name || 'john'
+    }
+
+    const jane = new Person('jane');
+    expect(jane.name).toBe('jane');
+  })
 })
 
