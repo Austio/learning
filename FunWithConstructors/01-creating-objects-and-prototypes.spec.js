7d24f57f9461241127b8b04fc6b0d3485a5c8d94
Austin Story <lonnieastory@gmail.com>
2017-11-08 06:46:39 -0600

    Creating Function constructor gotcha

--- FunWithConstructors/01-creating-objects-and-prototypes.spec.js
+++ FunWithConstructors/01-creating-objects-and-prototypes.spec.js
@@ -1,33 +1,39 @@
 describe('constructors', () => {
-  it('implicitly returns this when nothing returned', () => {
-    function Person() {
-      this.name = 'john'
-    }
+  describe('behavior', () => {
+    it('implicitly returns this when nothing returned', () => {
+      function Person() {
+        this.name = 'john'
+      }
 
-    const john = new Person();
+      const john = new Person();
 
-    expect(typeof john).toBe('object');
-    expect(john.name).toBe('john');
+      expect(typeof john).toBe('object');
+      expect(john.name).toBe('john');
+    });
+
+    it('does not respect the return word', () => {
+      function Person() {
+        this.name = 'john'
+
+        return 1;
+      }
+
+      const john = new Person();
+      expect(john.name).toBe('john');
+    });
+
+    it('allows arguments', () => {
+      function Person(name) {
+        this.name = name || 'john'
+      }
+
+      const jane = new Person('jane');
+      expect(jane.name).toBe('jane');
+    })
   });
-
-  it('does not respect the return word', () => {
-    function Person() {
-      this.name = 'john'
-
-      return 1;
-    }
-
-    const john = new Person();
-    expect(john.name).toBe('john');
-  });
-
-  it('allows arguments', () => {
-    function Person(name) {
-      this.name = name || 'john'
-    }
-
-    const jane = new Person('jane');
-    expect(jane.name).toBe('jane');
+  describe('Function constructor', () => {
+    it('creates in global context and does not respect closure')
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function
   })
 })
 
