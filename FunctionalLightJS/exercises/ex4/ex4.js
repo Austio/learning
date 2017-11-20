8d682d7699cd5d33c04b972af5dfbcf7fabb3513
Austin <lonnieastory@gmail.com>
2017-11-20 06:27:07 -0600

    Freeze in the pick instead of intnernal to pick

--- FunctionalLightJS/exercises/ex4/ex4.js
+++ FunctionalLightJS/exercises/ex4/ex4.js
@@ -3,20 +3,20 @@ function lotteryNum() {
 }
 
 function pick(n, l = []) {
-  Object.freeze(l);
 	if (n === 0) {
   	return l;
 	}
-
 
 	const num = lotteryNum();
   if (l.indexOf(num) > -1) {
   	return pick(n, l);
 	}
 
-	const newList = [...l, num].sort((l, r) => Number(l) > Number(r));
-	return pick(n - 1, newList);
+	console.warn(num)
+	console.log(l)
+	const newList = [...l, num].sort((l, r) => l - r);
+	return pick(n - 1, Object.freeze(newList));
 }
 
 
-console.log(pick(9));
+console.log(pick(11));
