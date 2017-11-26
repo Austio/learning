a78dbbf92f9cab3877fe0a482bf1ae1c519d3c0d
Austin <lonnieastory@gmail.com>
2017-11-26 13:40:18 -0600

    Recursive solution

--- FunctionalLightJS/exercises/ex6/ex6.js
+++ FunctionalLightJS/exercises/ex6/ex6.js
@@ -2,15 +2,30 @@ function mult(...args) {
   if (args.length < 1) {
     return 1;
   }
-	else if (args.length === 1) {
+
+  if (args.length === 1) {
 	  return args[0];
   } else {
 	  return args[0] * mult(...args.splice(1))
   }
 }
+//
+// console.log(mult(3))
+// console.log(mult(3,4))
+// console.log(mult(3,4,5));	// 60
+//
+// console.log(mult(3,4,5,6));	// Oops!
 
-console.log(mult(3))
-console.log(mult(3,4))
-console.log(mult(3,4,5));	// 60
+// With Kyle Idea Kyle
 
-console.log(mult(3,4,5,6));	// Oops!
+function mul(product, ...nums) {
+  if (nums.length === 0) {
+    return product || 1;
+  }
+
+  return mul(...nums) * product
+}
+console.log('3', mul(3))
+console.log('12', mul(3,4))
+console.log('60', mul(3,4,5));	// 60
+console.log('360', mul(3,4,5,6));	// Oops!
