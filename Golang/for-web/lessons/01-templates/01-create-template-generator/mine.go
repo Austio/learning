f0120ffc2f6d54d4eadaae2eefe59e40a158bac7
Austin Story <lonnieastory@gmail.com>
2017-12-04 05:14:43 -0600

    Super crude mustache syntax interpolation

--- Golang/for-web/lessons/01-templates/01-create-template-generator/mine.go
+++ Golang/for-web/lessons/01-templates/01-create-template-generator/mine.go
@@ -1,17 +1,32 @@
 // How would one create a template libary
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
 
-func mustache(template string, terms map[string]string) {
-	fmt.Println(template, terms)
+func interp(title string) {
+	fmt.Printf("<DOCTYPE html><head><title>%v<title></head><body>Hiya</body></html>", title)
+}
+
+func mustache(template string, dict map[string]string) {
+	fmt.Println("initial:", template, dict)
+
+	r, _ := regexp.Compile("{{title}}")
+	newTemplate := r.ReplaceAllString(template, dict["title"])
+
+	fmt.Println(newTemplate)
 }
 
 func main() {
+	interp("Using printf!")
+	fmt.Printf("\n\n")
+
 	var terms = map[string]string{
-		"foo": "bar",
+		"title": "Using Mustache Syntax",
 	}
 
-	template := terms["foo"]
+	template := "<DOCTYPE html><head><title>{{title}}<title></head><body>Hiya</body></html>"
 	mustache(template, terms)
 }
