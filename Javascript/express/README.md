b9ae0d41d0e293c4bfa95e21153fcd7827d0a510
Austin Story <lonnieastory@gmail.com>
2018-01-14 14:52:51 -0600

    Update express server config

--- Javascript/express/README.md
+++
@@ -1,6 +0,0 @@
-
-while true; do curl localhost:3001/settimeout; sleep .5; done;
-
-while true; do curl localhost:3001/promise; done;
-
-curl localhost:3001/block
--- Javascript/express/index.js
+++ Javascript/express/index.js
@@ -1,3 +1,10 @@
+
+/*
+  while true; do curl localhost:3001/settimeout; sleep .5; done;
+  while true; do curl localhost:3001/promise; done;
+  curl localhost:3001/block
+*/
+
 const app = require('express')();
 
 let exec = 0;
