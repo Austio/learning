50e58d0f7dbe70b071aa6e3116424c368017b063
Austin Story <lonnieastory@gmail.com>
2018-01-22 07:25:46 -0600

    Golang passing struct/slice/structs to display title and people

--- Golang/for-web/lessons/07-templ-data-structures/main.go
+++ Golang/for-web/lessons/07-templ-data-structures/main.go
@@ -37,8 +37,44 @@ func demoMap() {
 	tmpl.ExecuteTemplate(os.Stdout, "map.gohtml", d)
 }
 
+type Person struct {
+	Name   string
+	Saying string
+}
+
+var a = Person{
+	Name:   "jim",
+	Saying: "hiya",
+}
+
+var b = Person{
+	Name:   "john",
+	Saying: "yepper",
+}
+
+var ppl = []Person{a, b}
+
+func demoSliceStruct() {
+	tmpl.ExecuteTemplate(os.Stdout, "structSlice.gohtml", ppl)
+}
+
+type html struct {
+	Title  string
+	People []Person
+}
+
+func demoStructSliceStruct() {
+	d := html{
+		Title:  "StructSlicStruct",
+		People: ppl,
+	}
+
+	tmpl.ExecuteTemplate(os.Stdout, "structSliceStruct.gohtml", d)
+}
+
 func main() {
 	demoStruct()
 	demoMap()
-
+	demoSliceStruct()
+	demoStructSliceStruct()
 }
