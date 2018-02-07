e36e8984601b01fae92dbc972bfae1152ff33d26
Austin Story <lonnieastory@gmail.com>
2018-02-07 07:08:18 -0600

    Adding open/close connection in net package

--- Golang/for-web/lessons/15-tcp-server/01_write.go
+++ Golang/for-web/lessons/15-tcp-server/01_write.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	li, err := net.Listen("tcp", ":8071")
+	li, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Panic(err)
 	}
---
+++ Golang/for-web/lessons/15-tcp-server/02_read_write.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net"
+	"log"
+	"fmt"
+	"bufio"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		fmt.Fprintf(conn, "I heard you say: %s\n", ln)
+	}
+	defer conn.Close()
+
+	fmt.Println("Code got here")
+}
