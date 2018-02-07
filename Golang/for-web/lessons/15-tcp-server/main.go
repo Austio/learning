b3840ca6ae6cbb8ff4958564fdf8a416b6fab768
Austin Story <lonnieastory@gmail.com>
2018-02-07 06:49:11 -0600

    GoLange base tcp server example

---
+++ Golang/for-web/lessons/15-tcp-server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"io"
+	"net"
+	"log"
+	"fmt"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8071")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+
+		io.WriteString(conn, "\nHowdy\n")
+		fmt.Fprintln(conn, "How is your day?")
+		fmt.Fprintf(conn, "%v", "Well, I Hope")
+
+		conn.Close()
+	}
+}
