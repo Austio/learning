83c4e2685742c6816a900f69794122f7b21f4710
Austin Story <lonnieastory@gmail.com>
2018-02-08 06:35:59 -0600

    GOLang add dial example for reaching out to go server

---
+++ Golang/for-web/lessons/15-tcp-server/03_dial.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"bufio"
+)
+
+func main() {
+  	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+
+	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+	status, err := bufio.NewReader(conn).ReadString('\n')
+	fmt.Println(status)
+}
---
+++ Golang/for-web/lessons/15-tcp-server/TCP_SERVER.md
@@ -0,0 +1,7 @@
+in this director
+```
+go run 01_write.go
+
+# In another terminal
+go run 03_dial.go
+```
