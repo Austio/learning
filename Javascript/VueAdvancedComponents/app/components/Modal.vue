bb6d9945442a122a8ec8cf34469334701932d65a
Austin Story <lonnieastory@gmail.com>
2019-04-04 21:37:22 -0500

    Add some basic CSS styles for layout of nuxt app

---
+++ Javascript/VueAdvancedComponents/app/components/Modal.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    This is a modal
+    <button>Close</button>
+    <button>Confirm</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal"
+};
+</script>
+
+<style scoped></style>
---
+++ Javascript/VueAdvancedComponents/app/pages/global_management.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    HIYA
+    <Modal />
+  </div>
+</template>
+
+<script>
+import Modal from "~/components/Modal";
+
+export default {
+  name: "GlobalManagement",
+  components: {
+    Modal
+  }
+};
+</script>
