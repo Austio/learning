9c1d40af67c399c019d4d102ead21cd94973ecc5
Austin Story <lonnieastory@gmail.com>
2019-04-18 23:13:13 -0500

    Elasticsearch RelevantSearch chapter 2, going back and resummarizing

--- ElasticSearch/relevant-search/02_SearchUnderTheHood.md
+++ ElasticSearch/relevant-search/02_SearchUnderTheHood.md
@@ -1,5 +1,26 @@
 ## Chapter 2 - Search Under the Hood
 
+Search is a conversation between user and search engine
+ - user attempts to satisfy an information need through constaints
+ - search engine uses constraints to match and provide results
+
+Search engine is mechanical, it is a dumb token matching
+
+Science: Knowing how to extract features from queries and content
+Art: What constitutes a good descriptive feature
+
+
+### The Document
+Documents are items being stored, searched and returned.  
+Documents contain a set of fields named attributes of the document.
+
+Strings are normally searched within (title, name, etc)
+
+
+
+
+
+
 Analysis of a query has 3 steps
  - character filtering: ability to modify entire piece of text
  - tokenizing: chops up into stream of tokens
--- Projects/BuildSQLite/clang/db.h
+++ Projects/BuildSQLite/clang/db.h
@@ -112,5 +112,29 @@ struct Cursor_t {
 };
 typedef struct Cursor_t Cursor;
 
+enum NodeType_t { NODE_INTERNAL, NODE_LEAF };
+typedef enum NodeType_t NodeType;
+
+// Common Node Header
+const uint32_t NODE_TYPE_SIZE = sizeof(uint8_t);
+const uint32_t NODE_TYPE_OFFSET = 0;
+const uint32_t IS_ROOT_SIZE = sizeof(uint8_t);
+const uint32_t IS_ROOT_OFFSET = NODE_TYPE_SIZE;
+const uint32_t PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
+const uint8_t COMMON_NODE_HEADER_SIZE = NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_OFFSET;
+
+// Leaf Node Header
+const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
+const uint32_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE;
+
+// Leaf Body
+const unint32_t LEAF_NODE_KEY_SIZE = sizeof(uint32_t);
+const unint32_t LEAF_NODE_KEY_OFFSET = 0;
+const unint32_t LEAF_NODE_VALUE_SIZE = ROW_SIZE;
+const unint32_t LEAF_NODE_VALUE_OFFSET = LEAF_NODE_KEY_OFFSET + LEAF_NODE_KEY_SIZE;
+const unint32_t LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
+const unint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
+const unint32_t LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
 
 #endif //LEARNING_REPL_H
